868c99ea0ba962b57c9853e839b06b03be06e283
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2023-08-18 20:38:12 +0530

    feat: input logic and api call function

--- src/components/heroSection/heroSection.jsx
+++ src/components/heroSection/heroSection.jsx
@@ -1,35 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./heroSection.module.css";
 import { useEffect } from "react";
 import axios from "axios";
 
 export const HeroSection = () => {
-    useEffect(() => {
-        console.log("hi")
-        const test = async () => {
-          const options = {
-            method: "GET",
-            url: "https://zillow56.p.rapidapi.com/search",
-            params: {
-              location: "555 Byron St APT 305, Palo Alto, CA 94301"
-            },
-            headers: {
-              "X-RapidAPI-Key":
-                "02f713baa7mshfdd0501cc1da63dp157545jsn068e9a5e31b3",
-              "X-RapidAPI-Host": "zillow56.p.rapidapi.com"
-            }
-          };
-    
-          try {
-            const response = await axios.request(options);
-            console.log(response);
-          } catch (error) {
-            console.error(error);
-          }
-        };
-        // test();
-      }, []);
-    
+  const [address, setAddress] = useState("");
+  const searchUserAddress = async () => {
+    const options = {
+      method: "GET",
+      url: "https://zillow56.p.rapidapi.com/search",
+      params: {
+        location: "555 Byron St APT 305, Palo Alto, CA 94301",
+      },
+      headers: {
+        "X-RapidAPI-Key": "02f713baa7mshfdd0501cc1da63dp157545jsn068e9a5e31b3",
+        "X-RapidAPI-Host": "zillow56.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const inputHandler = (value) => {
+    setAddress(value);
+  };
+  const searchHandler = () => {
+    console.log("hello");
+  };
+
   return (
     <div className=" flex justify-center px-10">
       <div
@@ -45,8 +47,18 @@ export const HeroSection = () => {
           </p>
         </div>
 
-        <div className={`${styles.inputBg} md:w-[75%] w-full p-4`}>
-          <input className=" bg-transparent outline-none " />
+        <div className={`${styles.inputBg} md:w-[75%] w-full p-4 flex`}>
+          <input
+            value={address}
+            onChange={(e) => inputHandler(e.target.value)}
+            placeholder="Enter an address"
+            className="bg-transparent outline-none w-full"
+          />
+          <img
+            onClick={searchHandler}
+            className="cursor-pointer"
+            src="/search.svg"
+          />
         </div>
       </div>
     </div>
